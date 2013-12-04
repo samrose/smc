2db8f00cc643e4e7b9857bf16b734dade295026d
vagrant <vagrant@precise32.(none)>
2013-12-04 13:17:01 +0000

    angular

--- src/model/blog_post.erl
+++ src/model/blog_post.erl
@@ -1,5 +1,6 @@
 -module(blog_post, [Id, ClassroomId, ParticipantId, Title, Body, CreatedOn, ModifiedOn]).
 -compile(export_all).
 
+-has({blog_post_comments, many}).
 -belongs_to(participant).
 -belongs_to(classroom).
